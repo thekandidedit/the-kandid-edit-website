f63ba819ab7b73a86bd9572890e2bb7162b222b0
thekandidedit <thekandidedit@gmail.com>
2025-08-28 13:03:14 +0530

    Delete app/components/ui directory

--- app/components/ui/Card.tsx
+++
@@ -1,3 +0,0 @@
-import * as React from 'react';
-export const Card: React.FC<React.HTMLAttributes<HTMLDivElement>> = ({ className='', ...props }) =>
-  <div className={`card ${className}`} {...props} />;
